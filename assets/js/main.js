c954ee3145adc5e5344dc90626d06f84ea6322b0
moudjane <mathis.oudjane@gmail.com>
2022-03-16 13:54:26 +0100

    caroussel

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+    // LEFT BUTTON
+function scrollLeftt (divisionquibouge) {
+        document.getElementById(divisionquibouge).scrollLeft -= 250;
+      };
+
+    //   RIGHT BUTTON
+function scrollRight (divisionquibouge) {
+      document.getElementById(divisionquibouge).scrollLeft += 250;
+    };
